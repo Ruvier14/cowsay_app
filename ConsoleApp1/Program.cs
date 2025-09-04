5388a6c20cd43c819a0d5129dce4196ceaed1de8
Rowe <ruvier20144@gmail.com>
2025-09-04 19:54:27 +0800

    adding cowsay code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter something for the cow to say: ");
+        string userInput = Console.ReadLine();
+
+        // Setup process to call cowsay through WSL
+        ProcessStartInfo psi = new ProcessStartInfo
+        {
+            FileName = "wsl",                  // Call WSL
+            Arguments = $"cowsay \"{userInput}\"", // cowsay inside WSL
+            RedirectStandardOutput = true,     // Capture output
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        try
+        {
+            using (Process process = Process.Start(psi))
+            {
+                string result = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+                Console.WriteLine(result);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error running cowsay: " + ex.Message);
+        }
+    }
+}
